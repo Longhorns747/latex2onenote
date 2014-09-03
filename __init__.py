a91a86acd5c0b40f93d4d33068a6060c67738a23
Ethan Shernan <eshernan3@gatech.edu>
2014-09-02 23:23:09 -0400

    Moving latexFiles to static dir

--- __init__.py
+++ __init__.py
@@ -8,16 +8,16 @@ def home():
 @app.route("/process", methods=['POST'])
 def process_latex():
     latex_input = request.form['latex_input']
-    f = open('latexFiles/latex.tex', 'w')
+    f = open('static/latexFiles/latex.tex', 'w')
     f.write(latex_input)
     f.close()
 
     bashCommand = "htlatex latex.tex"
     import subprocess
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='latexFiles')
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='static/latexFiles')
     output = process.communicate()[0]
 
-    f = open('latexFiles/latex.html')
+    f = open('static/latexFiles/latex.html')
     finalHTML = f.read()
 
     return finalHTML
