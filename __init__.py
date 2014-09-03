727375bf96e3b55204825021b327177e287a96b6
Ethan Shernan <eshernan3@gatech.edu>
2014-09-02 22:33:05 -0400

    Echoing message from the console

--- __init__.py
+++ __init__.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    bashCommand = "echo 'Hello World!'"
+    import subprocess
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+    return render_template("index.html", output=output)
 
 if __name__ == "__main__":
     app.run(debug="true")
