b5bc30a84ce05b381f2ab9c2567501b839323fae
Ethan Shernan <eshernan3@gatech.edu>
2014-09-03 09:01:35 -0400

    Creating directory if not exists

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,13 +9,19 @@ def home():
 @app.route("/process", methods=['POST'])
 def process_latex():
     latex_input = request.form['latex_input']
+    staticFilepath = 'static/latexFiles'
+
+    d = os.path.dirname(staticFilepath)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
     f = open('static/latexFiles/latex.tex', 'w')
     f.write(latex_input)
     f.close()
 
     bashCommand = "htlatex latex.tex"
     import subprocess
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='static/latexFiles')
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd=staticFilepath)
     output = process.communicate()[0]
 
     f = open('static/latexFiles/latex.html')
