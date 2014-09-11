06fc49cfe43c01e0ee84c3bc71648ebc67a5a0ed
Ethan Shernan <eshernan3@gatech.edu>
2014-09-11 08:11:51 -0400

    Commenting code, adding section name parameter

--- __init__.py
+++ __init__.py
@@ -11,16 +11,23 @@ def home():
 def process_latex():
     latex_input = request.form['latex_input']
     accessToken = request.form['access_token']
-    staticFilepath = 'static/latexFiles/' + random.choice(string.letters) + random.choice(string.letters) +'/'
+
+    #Make a randomly generated directory for latex compilation
+    staticFilepath = 'static/latexFiles/' + random.choice(string.letters) + random.choice(string.letters) + random.choice(string.letters) + '/'
 
     d = os.path.dirname(staticFilepath)
     if not os.path.exists(d):
         os.makedirs(d)
 
+    #If directory already exists, throw an error
+    else:
+        return render_template("error.html")
+
     f = open(staticFilepath + '/latex.tex', 'w')
     f.write(latex_input)
     f.close()
 
+    #Compile LaTeX
     bashCommand = "htlatex latex.tex"
     import subprocess
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd=staticFilepath)
@@ -29,7 +36,8 @@ def process_latex():
     f = open(staticFilepath + '/latex.html')
     finalHTML = f.read()
 
-    url = "https://www.onenote.com/api/v1.0/pages"
+    #Form POST request to the OneNote API
+    url = "https://www.onenote.com/api/v1.0/pages?sectionName=LaTeX_Notes"
     headers = {'Content-Type' : 'Text/html', 'Authorization' : 'Bearer ' + accessToken}
     r = requests.post(url, data=finalHTML, headers=headers)
     jsonResponse = r.json()
@@ -47,6 +55,7 @@ def microsoft_response():
     f = open('static/client_secret.cfg', 'r+')
     clientSecret = f.read()
 
+    #Exchange authentication code for access token
     url = 'https://login.live.com/oauth20_token.srf'
     headers = {'Content-Type' : 'application/x-www-form-urlencoded'}
     urlData = {'client_id' : '0000000044127D98', 'redirect_uri' : 'http://latex2onenote.cloudapp.net/latex_input', 'client_secret' : clientSecret, 'code' : authCode, 'grant_type' : 'authorization_code'}
