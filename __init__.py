7a16efbab58807681850727f831cba264097ddea
Ethan Shernan <eshernan3@gatech.edu>
2014-09-11 11:38:11 -0400

    Adding CSRF protection

--- __init__.py
+++ __init__.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import requests, random, string
 import os
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                  for x in xrange(32))
+    session['state'] = state
+    return render_template("index.html", state=state)
+
+@app.route('/latex_input', methods=['GET', 'POST'])
+def microsoft_response():
+    authCode = request.args.get('code')
+
+    f = open('static/client_secret.cfg', 'r+')
+    clientSecret = f.read()
+
+    #Exchange authentication code for access token
+    url = 'https://login.live.com/oauth20_token.srf'
+    headers = {'Content-Type' : 'application/x-www-form-urlencoded'}
+    urlData = {'client_id' : '0000000044127D98', 'redirect_uri' : 'http://latex2onenote.cloudapp.net/latex_input', 'client_secret' : clientSecret, 'code' : authCode, 'grant_type' : 'authorization_code'}
+    r = requests.post(url, data=urlData, headers=headers)
+    jsonResponse = r.json()
+    accessToken = jsonResponse['access_token']
+    return render_template("latex_input.html", access_token=accessToken)
 
 @app.route("/process", methods=['POST'])
 def process_latex():
+    if request.args.get('state', '') != session['state']:
+        return render_template("error.html", message="Hmmm, the state seems to be off here!")
+
     latex_input = request.form['latex_input']
     accessToken = request.form['access_token']
 
@@ -21,7 +43,7 @@ def process_latex():
 
     #If directory already exists, throw an error
     else:
-        return render_template("error.html")
+        return render_template("error.html", message="Oh no D:! Something went wrong :( Please try again!")
 
     f = open(staticFilepath + '/latex.tex', 'w')
     f.write(latex_input)
@@ -48,21 +70,6 @@ def process_latex():
 
     return render_template("success.html", onenote_url=link)
 
-@app.route('/latex_input', methods=['GET', 'POST'])
-def microsoft_response():
-    authCode = request.args.get('code')
-
-    f = open('static/client_secret.cfg', 'r+')
-    clientSecret = f.read()
-
-    #Exchange authentication code for access token
-    url = 'https://login.live.com/oauth20_token.srf'
-    headers = {'Content-Type' : 'application/x-www-form-urlencoded'}
-    urlData = {'client_id' : '0000000044127D98', 'redirect_uri' : 'http://latex2onenote.cloudapp.net/latex_input', 'client_secret' : clientSecret, 'code' : authCode, 'grant_type' : 'authorization_code'}
-    r = requests.post(url, data=urlData, headers=headers)
-    jsonResponse = r.json()
-    accessToken = jsonResponse['access_token']
-    return render_template("latex_input.html", access_token=accessToken)
-
+app.secret_key = open('static/app_key.cfg', 'r+').read()
 if __name__ == "__main__":
     app.run(debug="true")
